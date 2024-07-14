f1dfc814fb14be04c53bdcbc8e0ddb33600069a1
Macraen <macraen.dev@gmail.com>
2024-07-14 19:46:53 +0300

    Initialization

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\LogWebhookRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(LogWebhookRequests::class);
 //        $middleware->validateCsrfTokens(except: [
 //            'you-webhook-endpoint/action-name'
 //        ]);
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Middleware\LogWebhookRequests;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('webhook', App\Http\Controllers\WebHookController::class)->name('webhook.receive');
+Route::get('webhook', App\Http\Controllers\WebHookController::class)
+    ->name('webhook.receive')
+    ->middleware(LogWebhookRequests::class);
