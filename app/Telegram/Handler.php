4f7650fb7ad608014a2afad701b53188a89b0703
Macraen <macraen.dev@gmail.com>
2024-07-16 00:05:53 +0300

    Initialization

--- app/Telegram/Handler.php
+++ app/Telegram/Handler.php
@@ -2,12 +2,36 @@
 
 namespace App\Telegram;
 
+use DefStudio\Telegraph\Facades\Telegraph;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
+use DefStudio\Telegraph\Keyboard\Button;
+use DefStudio\Telegraph\Keyboard\Keyboard;
+use Illuminate\Support\Stringable;
 
 class Handler extends WebhookHandler
 {
+    public function start(): void
+    {
+        Telegraph::message('hello world')
+            ->keyboard(Keyboard::make()->buttons([
+                Button::make("🗑️ Delete")->action("delete")->param('id', $notification->id),
+                Button::make("📖 Mark as Read")->action("read")->param('id', $notification->id),
+                Button::make("👀 Open")->url('https://test.it'),
+            ])->chunk(2))->send();
+
+    }
     public function profile(): void
     {
         $this->reply('ПРОБИЛА');
+    }
+
+    protected function handleUnknownCommand(Stringable $text): void
+    {
+        $this->reply('Невідома команда (');
+    }
+
+    protected function handleChatMessage(Stringable $text): void
+    {
+        $this->reply('Не розумію про що ти )');
     }
 }
