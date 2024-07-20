b96c4db461057c6109042e7d2e5121979d7724cd
Macraen <macraen.dev@gmail.com>
2024-07-20 20:19:09 +0300

    DB settings

--- database/migrations/2024_07_16_203516_create_crypto_addresses_table.php
+++ database/migrations/2024_07_16_203516_create_crypto_addresses_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('crypto_addresses', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('chat_id')->index();
-            $table->foreign('chat_id')->references('id')->on('telegraph_chats')->onDelete('cascade');
+            $table->foreign('chat_id')->references('chat_id')->on('telegraph_chats')->onDelete('cascade');
             $table->string('currency');
             $table->string('address');
             $table->timestamps();
